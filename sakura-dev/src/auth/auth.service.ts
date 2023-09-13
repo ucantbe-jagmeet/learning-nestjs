b4e30621d5a5ea52696346fcbda7b77cb7172829
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-09-13 06:57:38 +0530

    validate function completed

--- sakura-dev/src/auth/auth.service.ts
+++ sakura-dev/src/auth/auth.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
-import { Auth } from 'src/models/auth.schema';
-import { InjectModel } from '@nestjs/mongoose';
-
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectModel(Auth.name) private readonly authModel: Model<Auth>,
-  ) {}
+  constructor(private readonly userService: UserService) {}
 
-  async validateUser(username: string, password: string) {}
+  async validateUser(username: string, password: string) {
+    const user = await this.userService.findOneWithUserName(username);
+
+    const isValidUser = await bcrypt.compare(password, user.password);
+    if (user && isValidUser) {
+      const { password, ...result } = user;
+      return result; // getting only result from user
+    }
+    return null;
+  }
 }

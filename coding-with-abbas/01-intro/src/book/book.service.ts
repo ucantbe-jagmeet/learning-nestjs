26ba689c97acb9a534ff169040cedc0a194a531e
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-09-24 16:04:11 +0530

    create and findAll function

--- coding-with-abbas/01-intro/src/book/book.service.ts
+++ coding-with-abbas/01-intro/src/book/book.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Book } from './schemas/book.schema';
+import * as mongoose from 'mongoose';
 
-@Injectable()
-export class BookService {}
+@Injectable() // this is decorator
+export class BookService {
+  constructor(
+    @InjectModel(Book.name)
+    private bookModel: mongoose.Model<Book>,
+  ) {}
+
+  async findAll(): Promise<Book[]> {
+    const books = await this.bookModel.find();
+    return books;
+  }
+  async create(book: Book): Promise<Book> {
+    const res = await this.bookModel.create(book);
+    return res;
+  }
+}

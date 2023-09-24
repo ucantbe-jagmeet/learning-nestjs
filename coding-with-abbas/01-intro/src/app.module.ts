e400a2a6de627884fc6d27e1e0f190c2af4def57
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-09-24 15:34:55 +0530

    setting up book module and env file

--- coding-with-abbas/01-intro/src/app.module.ts
+++ coding-with-abbas/01-intro/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

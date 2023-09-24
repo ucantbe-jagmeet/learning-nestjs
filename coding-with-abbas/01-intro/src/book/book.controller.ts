86e9c9d5975e01f3846bb3204e5eb4f067915df7
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-09-24 16:46:22 +0530

    other routes created

--- coding-with-abbas/01-intro/src/book/book.controller.ts
+++ coding-with-abbas/01-intro/src/book/book.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('/books')
 export class BookController {
@@ -14,8 +24,35 @@ export class BookController {
   @Post()
   async createBook(
     @Body()
-    book,
+    book: CreateBookDto,
   ): Promise<Book> {
     return this.bookService.create(book);
+  }
+
+  @Get(':id')
+  async getBook(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookService.findById(id);
+  }
+
+  @Put(':id')
+  async updateBook(
+    @Param('id')
+    id: string,
+
+    @Body()
+    book: UpdateBookDto,
+  ): Promise<Book> {
+    return this.bookService.updateById(id, book);
+  }
+
+  @Delete(':id')
+  async deleteBook(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookService.deleteById(id);
   }
 }
--- coding-with-abbas/01-intro/src/book/book.service.ts
+++ coding-with-abbas/01-intro/src/book/book.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './schemas/book.schema';
 import * as mongoose from 'mongoose';
@@ -17,5 +17,23 @@ export class BookService {
   async create(book: Book): Promise<Book> {
     const res = await this.bookModel.create(book);
     return res;
+  }
+  async findById(id: string): Promise<Book> {
+    const book = await this.bookModel.findById(id);
+    if (!book) {
+      throw new NotFoundException('Book Not Found');
+    }
+    return book;
+  }
+
+  async updateById(id: string, book: Book): Promise<Book> {
+    return await this.bookModel.findByIdAndUpdate(id, book, {
+      new: true,
+      runValidators: true,
+    });
+  }
+
+  async deleteById(id: string): Promise<Book> {
+    return await this.bookModel.findByIdAndDelete(id);
   }
 }

5505fe094ee0bff729a77e916e11fb978a61662f
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-09-25 19:53:17 +0530

    nestjs pipes validation setup

--- coding-with-abbas/01-intro/src/book/dto/create-book.dto.ts
+++ coding-with-abbas/01-intro/src/book/dto/create-book.dto.ts
@@ -1,9 +1,24 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Category } from '../schemas/book.schema';
 
 export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
   readonly title: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly author: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   readonly price: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly category: Category;
 }
--- coding-with-abbas/01-intro/src/book/dto/update-book.dto.ts
+++ coding-with-abbas/01-intro/src/book/dto/update-book.dto.ts
@@ -1,9 +1,24 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { Category } from '../schemas/book.schema';
 
 export class UpdateBookDto {
+  @IsNotEmpty()
+  @IsString()
   readonly title: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly author: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly price: string;
+
+  @IsNotEmpty()
+  @IsEnum(Category, { message: 'Please enter correct category' })
   readonly category: Category;
 }
